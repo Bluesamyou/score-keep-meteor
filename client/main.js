1c7461df2735f56826bf331bedcd4c0080cf2744
Shevi <shevids@gmail.com>
2020-04-07 08:58:19 +1000

    Created content with React

--- client/main.js
+++ client/main.js
@@ -1,1 +1,10 @@
-console.log("Loaded client side JS");
+import React from "react";
+import ReactDom from "react-dom";
+import { Meteor } from "meteor/meteor";
+
+const players = [];
+
+Meteor.startup(function () {
+  let jsx = <h1>This is a heading</h1>;
+  ReactDom.render(jsx, document.getElementById("app"));
+});
