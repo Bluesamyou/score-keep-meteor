beaac905bb5c164caa16764dfca943592de40a20
Shevi <shevids@gmail.com>
2020-04-07 23:17:53 +1000

    Testing  with react hooks

--- client/main.js
+++ client/main.js
@@ -2,9 +2,11 @@ import React from "react";
 import ReactDom from "react-dom";
 import { Meteor } from "meteor/meteor";
 
-const players = [];
+const App = () => {
+  return <h1>this is the new heading</h1>;
+};
 
 Meteor.startup(function () {
   let jsx = <h1>This is a heading</h1>;
-  ReactDom.render(jsx, document.getElementById("app"));
+  ReactDom.render(<App />, document.getElementById("app"));
 });
